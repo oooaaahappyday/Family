a885b7506caf80d2d090f50d8ecfd1348cbfa69c
oooaaahappyday <joseph.boisselier@gmail.com>
2017-10-10 11:41:21 +0200

    ajout bootstrap

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+// CSS
 import './styles/app.css' // Import du style
 
+// Config
 import angular from 'angular'
 import '@uirouter/angularjs'
 import config from './app.config.js'
+import 'bootstrap'
+
 //Controllers
 import FamillesController from './familles/familles.controller.js'
 import HomeController from './home/home.controller.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -16,7 +17,16 @@ module.exports = {
   		filename: 'index.html',
   		template: './src/index.html'
   	}),
-  	new ExtractTextPlugin('styles.css')
+  	new ExtractTextPlugin('styles.css'),
+  	new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+      // In case you imported plugins individually, you must also require them here:
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+    })
   ],
 
   // Configuration des loaders
@@ -28,7 +38,16 @@ module.exports = {
             fallback: 'style-loader',
             use: 'css-loader'
           })
-        }
+        },
+        {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      }
       ]
   }
 };
